20d2480aef38c8081f253981fde0c4c0e509852a
danielparera <danielparera@users.noreply.github.com>
2015-05-27 04:01:50 +0200

    Create ponderateSynsetWraped.py

---
+++ ponderate/ponderateSynsetWraped.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os, subprocess
+import textwrap, argparse
+
+parserarg = argparse.ArgumentParser(
+     prog='ponderateSynset',
+     formatter_class=argparse.RawDescriptionHelpFormatter,
+     description=textwrap.dedent('''\
+         calculate ponderated vectors for specific synset
+         --------------------------------
+             example of use $python3 %(prog)s --synset synset [[--debug]]
+         '''))
+
+parserarg.add_argument('--debug', action='store_false', default='TRUE', help='to show aditional information')
+parserarg.add_argument('--synset', dest='synset', required=True, default='', type=str , help='input synsets (required)')
+
+args = parserarg.parse_args()
+
+debug = not(bool(args.debug))
+if debug:	deb="--debug"
+else:		deb=""
+
+syn = args.synset
+
+cmd = "python ponderacioSynset.py --synset "+syn+" "+deb
+
+result = subprocess.check_output(cmd, shell=True).strip()
+
+print "++++++++++++++++++++++"
+
+print result
