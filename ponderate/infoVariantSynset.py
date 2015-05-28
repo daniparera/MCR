33af7a590270ce9153564d3c685ad76b24986d74
danielparera <danielparera@users.noreply.github.com>
2015-05-28 19:39:57 +0200

    Create infoVariantSynset.py

---
+++ ponderate/infoVariantSynset.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import subprocess
+import textwrap, argparse
+
+parserarg = argparse.ArgumentParser(
+     prog='infoVariantSynset',
+     formatter_class=argparse.RawDescriptionHelpFormatter,
+     description=textwrap.dedent('''\
+         get variant info for specific synset
+         --------------------------------
+             example of use $python3 %(prog)s --synset synset [[--debug]]
+         '''))
+
+parserarg.add_argument('--synset', dest='synset', required=True, default='', type=str , help='input synsets (required)')
+
+args = parserarg.parse_args()
+syn = args.synset
+
+cmd = "fgrep "+syn+" wei_eng-30_variant.tsv | nawk '{print $1,$2}'"
+tmp = subprocess.check_output(cmd, shell=True)
+
+variants = ''
+
+for variant in tmp.splitlines()[:-1]:
+
+	aux = variant.split()
+	variants = variants + aux[0] + "_" + aux[1] + "/"
+
+variant = tmp.splitlines()[-1]
+aux = variant.split()
+
+print variants + aux[0] + "_" + aux[1]
