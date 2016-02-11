e5b8dd7b8506259c49a3eded41314607ac95bfc6
danielparera <danielparera@users.noreply.github.com>
2016-02-11 05:56:52 +0100

    Added new parameter in updateMatrix.py and Added DB input files

--- Mappings/updateMatrix.py
+++ Mappings/updateMatrix.py
@@ -2,9 +2,8 @@
 
 import os
 import textwrap, argparse
-import pymysql
-import time
 
+from collections import OrderedDict
 from collections import defaultdict
 
 #import sys  
@@ -38,25 +37,22 @@ if __name__ == '__main__':
 	parserarg.add_argument('--file_matrix', dest='file_matrix', required=True, type=str , help='matrix\'s file (required)')
 	parserarg.add_argument('--file_lexicon', dest='file_lexicon', required=True, type=str , help='resource lexicon\'s file (required)')
 	parserarg.add_argument('--new_field_name', dest='nfn', required=True, type=str , help='new field name (required)')
+	parserarg.add_argument('--cross_lingual', dest='cl', action='store_false', default='TRUE', help='to append only coincidence')
 
 	args = parserarg.parse_args()
+	cl = not(bool(args.cl))
 
 	### Read and Create lexicon ################################################
 
 	input_file_lexicon = open(args.file_lexicon, "r")
+	line = input_file_lexicon.readline()
 
 	lexicon_dict = defaultdict(dict)
-	lexicon_dict_rest = defaultdict(dict)
 
 	for line in input_file_lexicon.readlines():
 
-		#print line
-		syn,word,conf = line.split()
+		syn,word,conf = line.split()[:3]
 		lexicon_dict[syn][word] = round(float(conf),2)
-
-	#print lexicon_dict
-	#time.sleep(10)
-
 
 	############################################################################
 
@@ -72,45 +68,50 @@ if __name__ == '__main__':
 
 	for line in input_file_matrix.readlines():
 
-		syn,word,csco = line.split()[:3]
-		#syn,word,csco = line.replace('\n', '').encode('iso-8859-1').split()[:3]
-		matrix[syn][word]['csco'] = csco
+		syn,word = line.split()[:2]
+		csco = float(line.split()[2])
 
 		# recovery other data present in the file, and store in matrix's data structure
 		for idx,elem in enumerate(line.split()[3:]):
 			matrix[syn][word][lex_names[idx]] = elem
 
-		# go through a lexicon_dict, try to find an element
+		# go through a lexicon_dict, try to find an element.
 		try:
 			matrix[syn][word]['new_field'] = lexicon_dict[syn].pop(word)
+			if csco <= 0 and not cl:
+				matrix[syn][word]['csco'] = csco - 1
+			else:
+				matrix[syn][word]['csco'] = csco
 				
 		except KeyError:
 			matrix[syn][word]['new_field'] = 0
-			#print syn + " - " + word
-			#print "NO TROBAR"
-			pass
+			matrix[syn][word]['csco'] = csco
 
 	#new_dict = {k:v for k,v in original_dict.items() if v}
 
+	# delete empty lists
 	for k in lexicon_dict.keys():
 		if not lexicon_dict[k]:
 			del lexicon_dict[k]
 
-	#print lexicon_dict
+	if not cl:
+		# add other elements not presents in original mcr matrix, with csco = -1
+		for syn,words in lexicon_dict.items():
 
-	for syn,words in lexicon_dict.items():
+			for word,conf in words.items():
 
-		for word,conf in words.items():
-
-			matrix[syn][word]['csco'] = "-1"
-			for field in lex_names:
-				matrix[syn][word][field] = 0
-			matrix[syn][word]['new_field'] = conf
+				matrix[syn][word]['csco'] = -1
+				for field in lex_names:
+					matrix[syn][word][field] = 0
+				matrix[syn][word]['new_field'] = conf
 
 	############################################################################
 
+	matrix_ord = OrderedDict(sorted(matrix.items(), key=lambda t: t[0], reverse=False))
+
 	### Output updated matrix ##################################################
 
+	# name for outpput file is the same that last but increment counter
 	i_output_name,o_output_name = args.file_matrix.split('_')
 	idx,f_output_name, = o_output_name.split('.')
 
@@ -120,9 +121,9 @@ if __name__ == '__main__':
 		output_file.write(key+'\t')
 	output_file.write('\n')
 
-	for syn,words in matrix.items():
+	for syn,words in matrix_ord.items():
 		for word,values in words.items():
-			output_file.write(syn+'\t'+word+'\t'+values['csco']+'\t')
+			output_file.write(syn+'\t'+word+'\t'+str(values['csco'])+'\t')
 			for key in lex_names:
 				output_file.write(str(values[key])+'\t')
 			output_file.write(str(values['new_field'])+'\n')
