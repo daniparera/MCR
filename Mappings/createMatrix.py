68b877b58335137b92a463985eb587578783682c
daniparera <daniparera@users.noreply.github.com>
2016-02-17 19:55:18 +0100

    Update createMatrix.py

--- Mappings/createMatrix.py
+++ Mappings/createMatrix.py
@@ -15,7 +15,7 @@ from collections import defaultdict
 
 if __name__ == '__main__':
 
-	parserarg = argparse.ArgumentParser(
+	argument_parser = argparse.ArgumentParser(
 	     prog='createMatrix.py',
 	     formatter_class=argparse.RawDescriptionHelpFormatter,
 	     description=textwrap.dedent('''\
@@ -25,14 +25,14 @@ if __name__ == '__main__':
 		     python createMatrix.py --host adimen.si.ehu.es --user guest --pwd guest --db mcr9 [[--language spa-30]] [[--language cat-30]] [[...]]
 		 '''))
 
-	parserarg.add_argument('--host', dest='host_db', required=True, type=str , help='host url\'s database (required)')
-	parserarg.add_argument('--user', dest='user_db', required=True, type=str , help='user\'s database (required)')
-	parserarg.add_argument('--pwd', dest='pwd_db', required=True, type=str , help='password\'s database (required)')
-	parserarg.add_argument('--db', dest='db_db', required=True, type=str , help='database\'s selection (required)')
+	argument_parser.add_argument('--host', dest='host_db', required=True, type=str , help='host url\'s database (required)')
+	argument_parser.add_argument('--user', dest='user_db', required=True, type=str , help='user\'s database (required)')
+	argument_parser.add_argument('--pwd', dest='pwd_db', required=True, type=str , help='password\'s database (required)')
+	argument_parser.add_argument('--db', dest='db_db', required=True, type=str , help='database\'s selection (required)')
 
-	parserarg.add_argument('--language', dest='lng', action='append', required=False, type=str , help='language (default all, optional)')
+	argument_parser.add_argument('--language', dest='lng', action='append', required=False, type=str , help='language (default all, optional)')
 
-	args = parserarg.parse_args()
+	args = argument_parser.parse_args()
 
 	lng = args.lng
 
@@ -44,12 +44,12 @@ if __name__ == '__main__':
 	cur = db.cursor(pymysql.cursors.DictCursor) 
 
 	# we build lists of languages.
+	languages_mcr = []
 	cur.execute("select * FROM wei_languages")
 	rows = cur.fetchall()
-
-	languages_mcr = []
 	for row in rows: languages_mcr.append(row["code"])
 
+	# if no introduce language parameter, all language. otherwise check that introduced language is in MCR
 	if lng is None: 
 		languages = languages_mcr
 	else: 
@@ -59,6 +59,7 @@ if __name__ == '__main__':
 				exit(1)
 		languages = lng
 
+	# create output dir if not exists
 	if not os.path.exists('out'):
 		os.makedirs('out')
 
@@ -68,11 +69,10 @@ if __name__ == '__main__':
 
 		matrix_dict = defaultdict(dict)
 
-		output_file = open('out/matrix-'+lang+'_0.tab', "w")
-
 		cur.execute("SELECT * FROM `wei_"+lang+"_variant`")
 		rows = cur.fetchall()
 
+		# for each row in SQL result extract synset-variant-csco tuple and put in a matrix
 		for row in rows:
 
 			syn = "-".join(row['offset'].split("-")[-2:]).decode('iso-8859-1').encode('utf-8')
@@ -80,12 +80,13 @@ if __name__ == '__main__':
 
 			matrix_dict[syn][word] = row['csco']
 
+		# create descriptor and write header to output file
+		output_file = open('out/matrix-'+lang+'_0.tab', "w")
 		output_file.write('synset\tword\tcsco\n')
 
-		print len(matrix_dict)
-
+		# for each synset-variant-csco tuple write data to a output file
 		for key,val in matrix_dict.items():
-			for word,csco in val.items():
-				output_file.write(key+'\t'+word+'\t'+str(csco)+'\n')
+			for variant,csco in val.items():
+				output_file.write(key+'\t'+variant+'\t'+str(csco)+'\n')
 
 		output_file.close()
